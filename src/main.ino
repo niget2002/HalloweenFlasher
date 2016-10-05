372e2071f0ddc9a1363d3af5a857932f178b0bb4
niget2002 <david@wallings.net>
2016-10-05 15:14:06 -0500

    Discovered how 'map' works. Also decided I can use the same pin that LED is on and kill two birds with one output

--- src/main.ino
+++ src/main.ino
@@ -5,44 +5,21 @@
   */
 
   #define aInput  A0
-  #define dOutput 02
   #define LED  13
 
   int sleepTime = 500;  //Default sleep time equates to half a second
   int maxSleep = 1000;  //Max sleep time = 1 second
-  int minSleep = 100;   //Min sleep time = 1/10 of a second
-  int maxVoltage = 5;   //vRef
-  int minVoltage = 0;   //Gnd
-
-  void calSleep();
-  void high();
-  void low();
+  int minSleep = 10;   //Min sleep time = 1/10 of a second
 
   void setup() {
     pinMode(LED, OUTPUT);
-    pinMode(dOutput, OUTPUT);
     pinMode(aInput, INPUT);
   }
 
   void loop(){
-    calSleep();
-    high();
-    delay(sleepTime);
-    low();
-    delay(sleepTime);
-  }
-
-  void calSleep(){    //Calculate sleep time based on analog input, max, and min values
-    float voltage = analogRead(aInput);
-    sleepTime=((maxSleep-minSleep)/(maxVoltage-minVoltage))*voltage+minSleep; //this assumes minVoltage is Gnd
-  }
-
-  void high(){
+    int sleepTime = map(analogRead(aInput), 0, 1023, minSleep, maxSleep);
     digitalWrite(LED, HIGH);
-    digitalWrite(dOutput, HIGH);
-  }
-
-  void low(){
+    delay(sleepTime);
     digitalWrite(LED, LOW);
-    digitalWrite(dOutput, LOW);
+    delay(sleepTime);
   }
