6495c209f8902b5b4dd24734b03e4876af65ef95
niget2002 <david@wallings.net>
2016-10-05 13:02:36 -0500

    added initial code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HalloweenFlasher
+
+Simple Strobe Effect using an Arduino.
+
+Uses a resistor/pot connected to 5v, A0 and GND as an input and uses D3 as an output to switch a relay on and off.
---
+++ src/main.ino
@@ -0,0 +1,41 @@
+/* HalloweenStrobe
+  Bought a little $20 strobe and decided it sucked.
+  Decided I could do better.
+  Spent $30 on hardware to make white Christmas lights act like a strobe
+  */
+
+  #define aInput  A0
+  #define dOutput 02
+  #define LED  13
+
+  int sleepTime = 500;  //Default sleep time equates to half a second
+  int maxSleep = 1000;  //Max sleep time = 1 second
+  int minSleep = 100;   //Min sleep time = 1/10 of a second
+
+  void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(dOutput, OUTPUT);
+    pinMode(aInput, INPUT);
+  }
+
+  void loop(){
+    calSleep();
+    high();
+    delay(sleepTime);
+    low();
+    delay(sleepTime);
+  }
+
+  void calSleep(){
+    int voltage = analogRead(aInput);
+    sleepTime=(maxSleep/minSleep)*voltage+minSleep;
+  }
+  void high(){
+    digitalWrite(LED, HIGH);
+    digitalWrite(dOutput, HIGH);
+  }
+
+  void low(){
+    digitalWrite(LED, LOW);
+    digitalWrite(dOutput, LOW);
+  }
