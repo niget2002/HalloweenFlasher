8a68c6afe7d2fc313e95e71b0709626d66a04641
niget2002 <david@wallings.net>
2016-10-07 13:01:24 -0500

    Trying to add Bluetooth support. I was bored and wanted to make it more interesting. Code currently untested

--- src/main.ino
+++ src/main.ino
@@ -4,22 +4,75 @@
   Spent $30 on hardware to make white Christmas lights act like a strobe
   */
 
+  #include <SoftwareSerial.h>
+
+  SoftwareSerial BT(10,11);
+
   #define aInput  A0
   #define LED  13
+  #define dOutput 8
 
   int sleepTime = 500;  //Default sleep time equates to half a second
   int maxSleep = 1000;  //Max sleep time = 1 second
   int minSleep = 10;   //Min sleep time = 1/10 of a second
 
+  void outLow();
+  void outHigh();
+
   void setup() {
     pinMode(LED, OUTPUT);
+    pinMode(dOutput, OUTPUT);
     pinMode(aInput, INPUT);
+    Serial.begin(9600);
+    BT.begin(9600);
   }
 
   void loop(){
-    int sleepTime = map(analogRead(aInput), 0, 1023, minSleep, maxSleep);
+
+    String t;
+    int on=1;
+    int sleepTime=1000;
+    int potOn=1;
+
+    if(potOn){
+      sleepTime = map(analogRead(aInput), 0, 1023, minSleep, maxSleep);
+    }
+    if(BT.available()){
+      while(BT.available() > 0){
+        t += (char)BT.read();
+      }
+      if(t == "on"){
+        on=1;
+      }
+      if(t == "off"){
+        outLow();
+        on=0;
+      }
+      if(t == "potOn"){
+        potOn=1;
+      }
+      if(t == "potOff"){
+        potOn=0;
+      }
+      else {
+        sleepTime = t.toInt();
+      }
+      t = "";
+    }
+    if(on){
+      outHigh();
+      delay(sleepTime);
+      outLow();
+      delay(sleepTime);
+    }
+  }
+
+  void outHigh(){
     digitalWrite(LED, HIGH);
-    delay(sleepTime);
+    digitalWrite(dOutput, HIGH);
+  }
+
+  void outLow(){
     digitalWrite(LED, LOW);
-    delay(sleepTime);
+    digitalWrite(dOutput, LOW);
   }
