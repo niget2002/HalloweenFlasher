b4fa04f003646706ad9b31ec4ba51f92753eb31f
niget2002 <david@wallings.net>
2016-10-19 12:52:02 -0500

    making 3 descreet outputs. removed bluetooth

---
+++ src/main2.ino
@@ -0,0 +1,66 @@
+/* HalloweenStrobe
+Bought a little $20 strobe and decided it sucked.
+Decided I could do better.
+Spent $30 on hardware to make white Christmas lights act like a strobe
+*/
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <EEPROM.h>
+
+SoftwareSerial BT(10,11);
+
+#define aInput  A0
+#define LED  13
+
+int dOutput[3] = {2, 3, 4};
+int sleepTime[3] = {500, 500, 500};  //Default sleep time equates to half a second
+int onTime[3] = {500, 500, 500};
+int maxSleep = 1000;  //Max sleep time = 1 second
+int minSleep = 10;   //Min sleep time = 1/10 of a second
+
+int sleepAddress = 0;
+int onAddress = 10;
+int potAddress = 20;
+unsigned long previousMillis[3] = {0, 0, 0};
+
+
+void setup() {
+  pinMode(dOutput[0], OUTPUT);
+  pinMode(dOutput[1], OUTPUT);
+  pinMode(dOutput[2], OUTPUT);
+  pinMode(aInput, INPUT);
+  Serial.begin(9600);
+  BT.begin(115200);
+  Serial.println("Hello");
+}
+
+void loop(){
+  String t;
+  int on;
+  int i;
+  EEPROM.get(onAddress,on);
+  EEPROM.get(sleepAddress,sleepTime[0]);
+  int potOn;
+  EEPROM.get(potAddress,potOn);
+  Serial.println(on);
+  Serial.println(potOn);
+  Serial.println(sleepTime[0]);
+  unsigned long currentMillis = 0;
+
+  while(1){
+    currentMillis = millis();
+    for(i=0; i <= 3; i++){
+      if(!digitalRead(dOutput[i])){
+        if(currentMillis - previousMillis[i] >= sleepTime[i]){
+          digitalWrite(dOutput[i], HIGH);
+          previousMillis[i] = currentMillis;
+        }
+      } else {
+        if(currentMillis - previousMillis[i] >= onTime[i]){
+          digitalWrite(dOutput[i], LOW);
+          previousMillis[i] = currentMillis;
+        }
+      }
+    }
+  }
+}
